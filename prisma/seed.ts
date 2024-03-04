eed2222252877672bb6c4933999970b56e3bca2a
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-04 18:02:53 +0000

    build: update seed to include a phrase

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,14 +2,20 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-type UserType = {
+type UserInputType = {
   email: string;
   username: string;
   password: string;
 };
 
+type PhraseInputType = {
+  content: string;
+  meaning: string;
+  example: string;
+};
+
 async function main() {
-  const user1: UserType = {
+  const user1: UserInputType = {
     email: 'max@test.com',
     username: 'max123',
     password: 'MostInnovativeAndConvolutedPassword',
@@ -17,6 +23,15 @@ async function main() {
 
   const max = await prisma.user.create({ data: user1 });
   console.log('user created:', max);
+
+  const phrase1: PhraseInputType = {
+    content: 'until the cows come home',
+    meaning: 'for a very long time',
+    example:
+      "I can't stand here until the cows come home, it's way too cold for that. Hurry!",
+  };
+  const untilTheCowsComeHome = await prisma.phrase.create({ data: phrase1 });
+  console.log('phrase created:', untilTheCowsComeHome);
 }
 
 main();
