7b401542f68528df91829493a6cf2992374bd94b
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-04 15:10:20 +0000

    docs: implement nest swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Learn phrases API')
+    .setDescription(
+      'An api for English learners (intermediate and up) to access resources, store their work, and access and store their progress'
+    )
+    .setVersion('1.0')
+    .addTag('phrases')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
