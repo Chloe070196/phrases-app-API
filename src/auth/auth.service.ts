5e7b558a934dab226fafc0cf58f69b0b833a2588
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-05 14:33:02 +0000

    feat: hashPassword method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  static async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 8);
+  }
+}
