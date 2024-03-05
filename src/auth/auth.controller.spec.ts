f37bc04b30026504b9161793c0059052bc48e6f7
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-05 19:00:23 +0000

    test: add missing providers/controllers

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
+import { PrismaService } from 'src/prisma.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, AuthService, UsersService],
       controllers: [AuthController],
     }).compile();
 
