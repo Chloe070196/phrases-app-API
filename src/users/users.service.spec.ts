7813c0a6b2c0df650cddd6e7fdd6efe3b5c37d5f
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-05 19:00:40 +0000

    test: getUserBy()

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from 'src/prisma.service';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthController } from 'src/auth/auth.controller';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, PrismaService, AuthService],
+      controllers: [AuthController],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getUserBy', () => {
+    it('returns a user', async () => {
+      prismaService.user.findUnique = jest.fn().mockReturnValueOnce({
+        username: 'joe123',
+        password: 'segboerg',
+        email: 'mock@email.com',
+      });
+      const email = 'mock@email.com';
+
+      const result = await service.getUserBy({ email });
+      expect(result).not.toBeUndefined();
+      expect(result?.username).toEqual('joe123');
+    });
   });
 });
