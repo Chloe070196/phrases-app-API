075d048b8b58e60f171c0ba3c05c9730e150cc84
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 10:38:31 +0000

    feat: save() phrase controller

--- src/phrases/phrases.controller.ts
+++ src/phrases/phrases.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { PhrasesService } from './phrases.service';
 
 @Controller('phrases')
-export class PhrasesController {}
+export class PhrasesController {
+  constructor(private phrasesService: PhrasesService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/')
+  async save(
+    @Body()
+    createPhraseDto: {
+      content: string;
+      meaning: string;
+      example: string;
+    },
+  ) {
+    return await this.phrasesService.create(createPhraseDto);
+  }
+}
