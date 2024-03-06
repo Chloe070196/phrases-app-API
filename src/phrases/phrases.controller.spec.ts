da074d029d394963f9456507566e29a8431f2055
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 10:21:49 +0000

    test: save a phrase

--- src/phrases/phrases.controller.spec.ts
+++ src/phrases/phrases.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PhrasesController } from './phrases.controller';
+import { PrismaService } from 'src/prisma.service';
+import { PhrasesService } from './phrases.service';
 
 describe('PhrasesController', () => {
   let controller: PhrasesController;
+  let prismaService: PrismaService;
+  let phraseService: PhrasesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [PhrasesService, PrismaService],
       controllers: [PhrasesController],
     }).compile();
 
@@ -14,5 +19,32 @@ describe('PhrasesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe(': save', () => {
+    it('should save a phrase and return it', async () => {
+      const mockPhraseInput = {
+        content: 'time flies',
+        meaning: 'time seems to pass so quickly that we do not notice it',
+        example:
+          "Wow, it's already midnight! Time flies by when you're having fun.",
+      };
+      const mockPhraseReturned = {
+        ...mockPhraseInput,
+        id: 1,
+        createAt: 'some time',
+        updatedAt: 'some time',
+      };
+      prismaService.phrase.create = jest
+        .fn()
+        .mockReturnValueOnce(mockPhraseReturned);
+      const result = await phraseService.save(mockPhraseInput);
+      expect(result).not.toBeUndefined();
+      expect(result.status).toEqual(201);
+      expect(result.data.content).toEqual('time flies');
+    });
+  });
+  describe(': getPhrases', () => {
+    it('should return a list of phrases', () => {});
   });
 });
