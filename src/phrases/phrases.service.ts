db5a0767bbf915176515c965c5b29cbbd22853d7
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-06 10:48:00 +0000

    feat: create phrase service

--- src/phrases/phrases.service.ts
+++ src/phrases/phrases.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class PhrasesService {}
+export class PhrasesService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(phrase: Prisma.PhraseCreateInput) {
+    return await this.prismaService.phrase.create({ data: phrase });
+  }
+}
