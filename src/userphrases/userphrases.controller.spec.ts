9a57c51aec536eda21b05352c42d761b0c2c0d42
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-14 14:15:23 +0000

    test: get userphrases filtered by category

--- src/userphrases/userphrases.controller.spec.ts
+++ src/userphrases/userphrases.controller.spec.ts
@@ -4,11 +4,41 @@ import { PrismaService } from 'src/prisma.service';
 import { UserphrasesService } from './userphrases.service';
 import { Status } from '@prisma/client';
 
-describe('UserphrasesController', () => {
+fdescribe('UserphrasesController', () => {
   let controller: UserphrasesController;
   let prismaService: PrismaService;
 
-  const mockUserPhraseInput = {
+  const mockPhraseInput1 = {
+    category: 'making small talk',
+    content: 'time flies',
+    meaning: 'time seems to pass so quickly that we do not notice it',
+    example:
+      "As I'm scrolling through my social media feed, time flies by faster than I realize. One moment, I'm checking out funny memes, and the next, it's already been an hour.",
+    shortExample:
+      "Wow, it's already midnight! Time flies by when you're having fun.",
+  };
+  const mockPhraseReturned1 = {
+    ...mockPhraseInput1,
+    id: 1,
+    createAt: 'some time',
+    updatedAt: 'some time',
+  };
+  const mockPhraseInput2 = {
+    category: 'talking about learning',
+    content: 'practice makes perfect',
+    meaning: 'the way to get better at something is to keep doing it',
+    example:
+      "I've not managed to have a full conversation in Spanish yet where I haven't relied on Google translate. But I'm attending lessongs, and I'll be spending the summer in Madrid, so I'm bound to improve a lot. As they say, practice makes perfect!",
+    shortExample: "I'm not really good at it yet, but practice makes perfect!",
+  };
+  const mockPhraseReturned2 = {
+    ...mockPhraseInput2,
+    id: 1,
+    createAt: 'some time',
+    updatedAt: 'some time',
+  };
+
+  const mockUserPhraseInput1 = {
     phraseId: 1,
     userId: 1,
     status: Status.WIP,
@@ -16,14 +46,30 @@ describe('UserphrasesController', () => {
     timesSeen: 8,
     timesUsed: 1,
   };
-  const mockUserPhraseReturned = {
-    ...mockUserPhraseInput,
+  const mockUserPhraseReturned1 = {
+    ...mockUserPhraseInput1,
     id: 1,
+    phrase: mockPhraseReturned1,
+    createAt: 'some time',
+    updatedAt: 'some time',
+  };
+  const mockUserPhraseInput2 = {
+    phraseId: 2,
+    userId: 1,
+    status: Status.WIP,
+    timesAttempted: 5,
+    timesSeen: 8,
+    timesUsed: 1,
+  };
+  const mockUserPhraseReturned2 = {
+    ...mockUserPhraseInput2,
+    id: 1,
+    phrase: mockPhraseReturned2,
     createAt: 'some time',
     updatedAt: 'some time',
   };
   const mockModifiedUserPhrase = {
-    ...mockUserPhraseReturned,
+    ...mockUserPhraseReturned1,
     status: Status.LEARNT,
   };
 
@@ -45,8 +91,8 @@ describe('UserphrasesController', () => {
     it('should save a userphrase and return it', async () => {
       prismaService.userPhrase.create = jest
         .fn()
-        .mockReturnValueOnce(mockUserPhraseReturned);
-      const result = await controller.save(mockUserPhraseInput);
+        .mockReturnValueOnce(mockUserPhraseReturned1);
+      const result = await controller.save(mockUserPhraseInput1);
       expect(result).not.toBeUndefined();
       expect(result).toHaveProperty('id');
       expect(result.status).toEqual('WIP');
@@ -56,7 +102,7 @@ describe('UserphrasesController', () => {
     it('should return a list of userphrases', async () => {
       prismaService.userPhrase.findMany = jest
         .fn()
-        .mockReturnValueOnce([mockUserPhraseReturned]);
+        .mockReturnValueOnce([mockUserPhraseReturned1]);
       const result = await controller.getMany('1', {
         category: 'Everyday Conversations',
       });
@@ -64,7 +110,23 @@ describe('UserphrasesController', () => {
       if (result) {
         expect(result[0]).not.toBeUndefined();
         if (result) {
-          expect(result[0].timesAttempted).toEqual(5);
+          expect(result.length).toEqual(1);
+        }
+      }
+    });
+    it('should return an unfiltered list of all userphrases', async () => {
+      prismaService.userPhrase.findMany = jest
+        .fn()
+        .mockReturnValueOnce([
+          mockUserPhraseReturned1,
+          mockUserPhraseReturned2,
+        ]);
+      const result = await controller.getMany('1');
+      expect(result).not.toBeUndefined();
+      if (result) {
+        expect(result[0]).not.toBeUndefined();
+        if (result) {
+          expect(result.length).toEqual(2);
         }
       }
     });
