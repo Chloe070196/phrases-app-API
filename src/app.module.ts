2695b17acdf60c5b78cbf3d903052065c4051929
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-14 17:23:22 +0000

    feat: text module is available to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,25 @@ import { PhrasesModule } from './phrases/phrases.module';
 import { UserphrasesService } from './userphrases/userphrases.service';
 import { UserphrasesController } from './userphrases/userphrases.controller';
 import { UserphrasesModule } from './userphrases/userphrases.module';
+import { TextsService } from './texts/texts.service';
+import { TextsController } from './texts/texts.controller';
+import { TextsModule } from './texts/texts.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PhrasesModule, UserphrasesModule],
-  controllers: [AppController, UserphrasesController],
-  providers: [AppService, UsersService, PrismaService, UserphrasesService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PhrasesModule,
+    UserphrasesModule,
+    TextsModule,
+  ],
+  controllers: [AppController, UserphrasesController, TextsController],
+  providers: [
+    AppService,
+    UsersService,
+    PrismaService,
+    UserphrasesService,
+    TextsService,
+  ],
 })
 export class AppModule {}
