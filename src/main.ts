d4cfaf6760df823529fb0af3be0cb1ba0068ea84
Chloe Zermatten <chloezermatten@gmail.com>
2024-03-16 19:36:14 +0000

    fix: amemd origin header

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .addTag('phrases')
     .build();
   const corsOptions: CorsOptions = {
-    origin: 'english-phrases-learning-app.vercel.app',
+    origin: 'https://english-phrases-learning-app.vercel.app',
   };
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
