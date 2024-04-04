42f7418d61553fe77317f0976092158a3d16bb53
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-04 16:38:56 +0100

    fix: rollback changes to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const config = new DocumentBuilder()
-  //   .setTitle('Learn phrases API')
-  //   .setDescription(
-  //     'An api for English learners (intermediate and up) to access resources, store their work, and access and store their progress',
-  //   )
-  //   .setVersion('1.0')
-  //   .addTag('phrases')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Learn phrases API')
+    .setDescription(
+      'An api for English learners (intermediate and up) to access resources, store their work, and access and store their progress',
+    )
+    .setVersion('1.0')
+    .addTag('phrases')
+    .build();
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://english-phrases-learning-app.vercel.app',
     methods: 'GET,POST,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     optionsSuccessStatus: 200,
@@ -22,8 +22,8 @@ async function bootstrap() {
     credentials: true,
   };
   app.enableCors(corsOptions);
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
